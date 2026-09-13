c9731561cbc7df2959208606120f19b6cdd420e4
Andreas <info@azettl.net>
2026-09-13 11:17:28 +0200

    Update duckwork.rb

--- Casks/duckwork.rb
+++ Casks/duckwork.rb
@@ -2,7 +2,7 @@ cask "duckwork" do
   version "0.3.0"
   sha256 "06da2887ea3097dfb084d7a97697561986b682ac0e4657af995a74cd8da74b22"
 
-  url "https://duckwork.ai/assets/downloads/Duckwork-#{version}.zip"
+  url "https://downloads.duckwork.ai/Duckwork-#{version}.zip"
   name "Duckwork"
   desc "Duckwork is a macOS menu bar app for thinking out loud. Talk through a problem with a global hotkey, and only get a response when you ask for one."
   homepage "https://duckwork.ai/"
